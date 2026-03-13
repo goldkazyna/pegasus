3fed30208dfb8792915c903acb5d69fde6890d42
Denis Dudnikov <goldkazyna@gmail.com>
2026-03-13 15:01:48 +0500

    fix: set Webhook running mode for nutgram

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use SergiX44\Nutgram\Nutgram;
+use SergiX44\Nutgram\RunningMode\Webhook;
 
 Route::post('/telegram/webhook', function (Nutgram $bot) {
     $secret = config('nutgram.secret');
     if ($secret && request()->header('X-Telegram-Bot-Api-Secret-Token') !== $secret) {
         abort(403);
     }
+
+    $bot->setRunningMode(Webhook::class);
     $bot->run();
 });
